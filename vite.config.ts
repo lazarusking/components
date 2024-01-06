4239ac8d6f6b6f1ae076515270d53cbc7b703c6f
lazarusking <vegetasssuper@gmail.com>
2024-01-06 23:42:50 +0000

    set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = { plugins: [react()], base: "/" };
+  const config: UserConfig = {
+    plugins: [react()],
+    base: "/",
+  };
   if (command !== "serve") {
     config.base = "/components/";
   }
