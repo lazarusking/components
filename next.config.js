5121c441dc6886e4a3397a7cb7d287817a286f84
lazarusking <vegetasssuper@gmail.com>
2024-01-14 14:44:23 +0000

    check env on next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   output: "export",
   // Configure `pageExtensions` to include MDX files
+  basePath: this.env == "production" ? "/components" : "",
+  assetPrefix: this.env == "production" ? "/components" : undefined,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
 };
